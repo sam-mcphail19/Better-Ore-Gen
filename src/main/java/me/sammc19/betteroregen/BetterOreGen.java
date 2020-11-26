8d0baf64a46aafcf6098cc6fe47aadeb285907cf
Sam McPhail <sam.mcphail9@gmail.com>
2020-11-26 14:48:17 -0500

    Initial commit

---
+++ src/main/java/me/sammc19/betteroregen/BetterOreGen.java
@@ -0,0 +1,11 @@
+package me.sammc19.betteroregen;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterOreGen implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
