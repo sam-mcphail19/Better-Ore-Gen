a2cc8abfd848eed62defab644cee6de4d2f627fc
Sam McPhail <sam.mcphail9@gmail.com>
2021-11-24 16:29:34 -0500

    Restrict entered frequencies to be greater than 0. Format invalid input messages

--- src/main/java/me/sammc19/betteroregen/config/OreVeinReader.java
+++ src/main/java/me/sammc19/betteroregen/config/OreVeinReader.java
@@ -16,7 +16,7 @@ public class OreVeinReader {
 
     private static final int WORLD_HEIGHT = 256;
 
-    public static OreVein readVein(JsonObject jsonObject){
+    public static OreVein readVein(JsonObject jsonObject) {
         OreVein oreVein = new OreVein.Builder()
                 .withBlocks(readBlocks(jsonObject.get("blocks")))
                 .withBlockWeights(readBlockWeights(jsonObject.get("blocks")))
@@ -29,25 +29,25 @@ public class OreVeinReader {
                 .withBiomes(readBiomes(jsonObject.get("biomes")))
                 .build();
 
-        if(oreVein.yMin > oreVein.yMax){
-            throw new IllegalArgumentException("Invalid yMin/yMax. yMax must be greater than yMin.");
+        if (oreVein.yMin > oreVein.yMax) {
+            throw new IllegalArgumentException("Invalid yMin/yMax: (" + oreVein.yMin + ", " + oreVein.yMax + "), yMax must be greater than yMin.");
         }
 
         return oreVein;
     }
 
-    private static ArrayList<Block> readBlocks(JsonElement jsonElement){
+    private static ArrayList<Block> readBlocks(JsonElement jsonElement) {
         JsonArray jsonArray = jsonElement.getAsJsonArray();
 
-        if(jsonArray.size() % 2 == 1){
-            throw new IllegalArgumentException("Invalid blocks array. Blocks array must be even length.");
+        if (jsonArray.size() % 2 == 1) {
+            throw new IllegalArgumentException("Invalid blocks array: " + jsonArray + ", blocks array must be even length.");
         }
 
         ArrayList<Block> blocks = new ArrayList<>();
 
-        for(int i=0; i< jsonArray.size(); i+=2){
+        for (int i = 0; i < jsonArray.size(); i += 2) {
             Block block = Registry.BLOCK.get(new Identifier(jsonArray.get(i).getAsString()));
-            if(block == Blocks.AIR){
+            if (block == Blocks.AIR) {
                 throw new IllegalArgumentException("Invalid blocks array. Block: \"" + jsonArray.get(i).getAsString() + "\" not found.");
             }
             blocks.add(block);
@@ -55,80 +55,84 @@ public class OreVeinReader {
         return blocks;
     }
 
-    private static ArrayList<Integer> readBlockWeights(JsonElement jsonElement){
+    private static ArrayList<Integer> readBlockWeights(JsonElement jsonElement) {
         JsonArray jsonArray = jsonElement.getAsJsonArray();
 
-        if(jsonArray.size() % 2 == 1){
-            throw new IllegalArgumentException("Invalid blocks array. Blocks array must be even length.");
+        if (jsonArray.size() % 2 == 1) {
+            throw new IllegalArgumentException("Invalid blocks array: " + jsonArray + ", blocks array must be even length.");
         }
 
         ArrayList<Integer> blockWeights = new ArrayList<>();
         int sum = 0;
 
-        for(int i=1; i< jsonArray.size(); i+=2){
+        for (int i = 1; i < jsonArray.size(); i += 2) {
             int blockWeight = jsonArray.get(i).getAsInt();
             blockWeights.add(blockWeight);
             sum += blockWeight;
         }
 
-        if(sum != 100){
-            throw new IllegalArgumentException("Invalid blocks array. Blocks must sum to 100.");
+        if (sum != 100) {
+            throw new IllegalArgumentException("Invalid blocks array: " + jsonArray + ", block weights must sum to 100.");
         }
         return blockWeights;
     }
 
-    private static ArrayList<Biome> readBiomes(JsonElement jsonElement){
+    private static ArrayList<Biome> readBiomes(JsonElement jsonElement) {
         JsonArray jsonArray = jsonElement.getAsJsonArray();
 
         ArrayList<Biome> biomes = new ArrayList<>();
 
-        for(int i=0; i< jsonArray.size(); i++){
+        for (int i = 0; i < jsonArray.size(); i++) {
             //biomes.add(jsonArray.get(i).getAsString());
         }
 
         return biomes;
     }
 
-    private static ArrayList<Integer> readDimensions(JsonElement jsonElement){
+    private static ArrayList<Integer> readDimensions(JsonElement jsonElement) {
         JsonArray jsonArray = jsonElement.getAsJsonArray();
 
         ArrayList<Integer> dimensions = new ArrayList<>();
 
-        for(int i=0; i< jsonArray.size(); i++){
+        for (int i = 0; i < jsonArray.size(); i++) {
             dimensions.add(jsonArray.get(i).getAsInt());
         }
 
         return dimensions;
     }
 
-    private static int readYMin(JsonElement jsonElement){
+    private static int readYMin(JsonElement jsonElement) {
         int yMin = jsonElement.getAsInt();
-        if(yMin < 0 || yMin > WORLD_HEIGHT){
-            throw new IllegalArgumentException("Invalid yMin. yMin must be between 0 and " + WORLD_HEIGHT + ".");
+        if (yMin < 0 || yMin > WORLD_HEIGHT) {
+            throw new IllegalArgumentException("Invalid yMin: " + yMin + ", yMin must be between 0 and " + WORLD_HEIGHT + ".");
         }
         return yMin;
     }
 
-    private static int readYMax(JsonElement jsonElement){
+    private static int readYMax(JsonElement jsonElement) {
         int yMax = jsonElement.getAsInt();
-        if(yMax < 0 || yMax > WORLD_HEIGHT){
-            throw new IllegalArgumentException("Invalid yMax. yMax must be between 0 and " + WORLD_HEIGHT + ".");
+        if (yMax < 0 || yMax > WORLD_HEIGHT) {
+            throw new IllegalArgumentException("Invalid yMax: " + yMax + ", yMax must be between 0 and " + WORLD_HEIGHT + ".");
         }
         return yMax;
     }
 
-    private static int readSize(JsonElement jsonElement){
+    private static int readSize(JsonElement jsonElement) {
         return jsonElement.getAsInt();
     }
 
-    private static float readFrequency(JsonElement jsonElement){
-        return jsonElement.getAsFloat();
+    private static float readFrequency(JsonElement jsonElement) {
+        float frequency = jsonElement.getAsFloat();
+        if (frequency < 0) {
+            throw new IllegalArgumentException("Invalid frequency: " + frequency + ", frequency must be positive");
+        }
+        return frequency
     }
 
-    private static float readDensity(JsonElement jsonElement){
+    private static float readDensity(JsonElement jsonElement) {
         float density = jsonElement.getAsFloat();
-        if(density < 0 || density > 1){
-            throw new IllegalArgumentException("Invalid density. density must be between 0 and 1.");
+        if (density < 0 || density > 1) {
+            throw new IllegalArgumentException("Invalid density: " + density + ", density must be between 0 and 1.");
         }
         return density;
     }
